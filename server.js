de2bd15e9a3a5b9b2abc4e79bbb05d4a1af21557
mrhayxinhun <mrhayxinhun@gmail.com>
2018-10-02 17:21:44 +0700

    add new project page to app

--- server.js
+++ server.js
@@ -42,7 +42,13 @@ app.get('/', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About age'
+    pageTitle: 'About page'
+  })
+})
+
+app.get('/project', (req, res) => {
+  res.render('project.hbs', {
+    pageTitle: 'Project page'
   })
 })
 
